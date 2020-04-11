6210d572036c36954e23e177e80992ba516058e2
nibir <biplob.asanibir@gmail.com>
2020-04-11 14:40:55 +0600

    show patients data

--- src/Components/NoMatch/NoMatch.js
+++ src/Components/NoMatch/NoMatch.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Typography } from '@material-ui/core';
 import { Link } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 const NoMatch = () => {
     return (
         <>
+            <Navbar/>
             <Typography variant="h1">
                 OOPS Page Not Found :(
             </Typography>
