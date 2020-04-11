b89314e9604c147269485365fd26b77fcc82724e
nibir <biplob.asanibir@gmail.com>
2020-04-11 12:53:40 +0600

    load all apppoint data from api

--- src/Components/DoctorDashboard/DoctorDashboard.js
+++ src/Components/DoctorDashboard/DoctorDashboard.js
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Grid, makeStyles } from '@material-ui/core';
 import StatusCard from '../StatusCard/StatusCard';
+import { useState } from 'react';
+import { apiURL } from '../../App';
 
 
 const useStyles = makeStyles((theme) => ({
@@ -10,12 +12,22 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 const DoctorDashboard = () => {
+    const [appointment, setappoinment] = useState(null)
     const classes = useStyles();
-    const statusCardItems = [{total: 29, text: "Pending Appointments", color: "#F1536E"},
-                             {total: 20, text: "Today’s Appointments", color: "#3DA5F4"},
-                             {total: 12, text: "Total Appointments", color: "#00C689"},
-                             {total: 72, text: "Total Patients", color: "#FDA006"}
-                            ]
+    const statusCardItems = [
+        {total: 29, text: "Pending Appointments", color: "#F1536E"},
+        {total: 20, text: "Today’s Appointments", color: "#3DA5F4"},
+        {total: 12, text: "Total Appointments", color: "#00C689"},
+        {total: 72, text: "Total Patients", color: "#FDA006"}
+    ]
+    useEffect(()=>{
+        fetch(apiURL+"/getallappointments")
+        .then(response => response.json())
+        .then(data => setappoinment(data))
+    },[])
+
+    console.log(appointment)
+    
 
     return (
         <>
